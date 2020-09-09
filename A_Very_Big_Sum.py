4df60bd639d98824269679dd78cce034df06df6c
Nayak, Abhishek <abhishek_nayak1@dell.com>
2020-09-09 20:40:26 +0530

    A Very Big Sum - Passed

---
+++ A_Very_Big_Sum.py
@@ -0,0 +1,25 @@
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the aVeryBigSum function below.
+def aVeryBigSum(ar, ar_count):
+    temp = 0
+    for n in range (ar_count):
+        temp = temp + ar[n]
+    return temp
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = aVeryBigSum(ar, ar_count)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
